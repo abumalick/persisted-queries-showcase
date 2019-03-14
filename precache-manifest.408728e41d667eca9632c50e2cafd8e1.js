e418e59208ce6e78395521e929af1a5faf726f6b
abumalick <abdullah.hilson@gmail.com>
2019-03-14 12:06:12 +0000

    Updates

---
+++ precache-manifest.408728e41d667eca9632c50e2cafd8e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/persisted-queries-showcase/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2e4a59c5ecb8b3d5ec82",
+    "url": "/persisted-queries-showcase/static/js/runtime~main.a5d8a171.js"
+  },
+  {
+    "revision": "5fb81d796d376a01537b",
+    "url": "/persisted-queries-showcase/static/js/main.cd4ff022.chunk.js"
+  },
+  {
+    "revision": "390fd71fd132c223c4b5",
+    "url": "/persisted-queries-showcase/static/js/2.1516b1e8.chunk.js"
+  },
+  {
+    "revision": "5fb81d796d376a01537b",
+    "url": "/persisted-queries-showcase/static/css/main.009b7d93.chunk.css"
+  },
+  {
+    "revision": "7fe54bcf01a814031301a9a2e4d5af4c",
+    "url": "/persisted-queries-showcase/index.html"
+  }
+];
